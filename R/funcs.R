379370f9f777416532d50b504ad9b884f6bce027
Alper Kucukural <alper.kucukural@umassmed.edu>
2020-05-14 15:17:43 -0400

    box, bar plot fix

--- R/funcs.R
+++ R/funcs.R
@@ -108,9 +108,10 @@ getVariationData <- function(inputdata = NULL,
     # Pick out the gene with this ID
     vardata <- inputdata[key, ]
     bardata <- as.data.frame(cbind(key, cols,
-         t(vardata[, cols]), conds) )
+                                   t(vardata[, cols]), as.character(conds)) )
     colnames(bardata) <- c("genename", "libs", "count", "conds")
     bardata$count <- as.numeric(as.character(bardata$count))
+    bardata$conds <- factor(bardata$conds)
     data <- rbind(bardata[bardata$conds == levels(bardata$conds)[1], ],
                   bardata[bardata$conds == levels(bardata$conds)[2], ])
     data$conds  <- factor(data$conds, levels = unique(data$conds))
