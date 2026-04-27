1efdcfe54fc9386b35103356ccd1789d42701b49
Alper Kucukural <alper@viascientific.com>
2026-04-27 15:52:03 -0400

    build: regenerate man/ via Roxygen 7.3.3 + style vignette R extract
    - Trailing-whitespace cleanup across all man/*.Rd
    - vignettes/DEBrowser.R reformatted (auto-extracted from .Rmd by knitr)
    - .gitignore + .Rbuildignore: ignore .positai (IDE marker), and gitignore
      build-artifact vignette HTML output

--- vignettes/DEBrowser.R
+++ vignettes/DEBrowser.R
@@ -1,15 +1,15 @@
-## ---- echo=FALSE, results="hide", warning=FALSE-------------------------------
+## ----echo=FALSE, results="hide", warning=FALSE--------------------------------
 suppressPackageStartupMessages({
-    library(gplots)
-    library(ggplot2)
-    library(shiny)
-    library(DT)
-    library(clusterProfiler)
-    library(DESeq2)
-    library(org.Hs.eg.db)
-    library(edgeR)
-    library(debrowser)
-    library(RColorBrewer)
-    library(annotate)
+  library(gplots)
+  library(ggplot2)
+  library(shiny)
+  library(DT)
+  library(clusterProfiler)
+  library(DESeq2)
+  library(org.Hs.eg.db)
+  library(edgeR)
+  library(debrowser)
+  library(RColorBrewer)
+  library(annotate)
 })
 
